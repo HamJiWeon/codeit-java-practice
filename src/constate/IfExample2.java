d07bbdec40ce24a78719ade403c1d052ccf92006
HamJiwon <skxo0039@gmail.com>
2026-05-06 17:19:36 +0900

    Java 조건문 - if, else - if의 중첩, else if(다중 분기) - switch, case

---
+++ src/constate/IfExample2.java
@@ -0,0 +1,21 @@
+package constate;
+
+public class IfExample2 {
+    public static void main(String[] args) {
+        int age =25;
+
+        if (age >= 20) {
+            if (age < 40) System.out.println("청년층입니다.");
+            else if (age < 65) System.out.println("중장년층입니다.");
+            else System.out.println("노년층입니다.");
+        } else if (age >= 17) {
+            System.out.println("고등학생입니다.");
+        } else if (age >= 14) {
+            System.out.println("중학생입니다.");
+        } else if (age >= 8) {
+            System.out.println("초등학생입니다.");
+        } else {
+            System.out.println("미취학 아동입니다.");
+        }
+    }
+}
---
+++ src/constate/SwitchExample.java
@@ -0,0 +1,22 @@
+package constate;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+        String gender = "M";
+
+        switch (gender) {
+            case "M": case "m": case "남": case "ㅡ":
+                System.out.println("남성입니다.");
+                break;
+
+            // java 14버전 이후 개선된 case문
+            case "F", "f", "여", "ㄹ":
+                System.out.println("여성입니다.");
+                break;
+
+            default:
+                System.out.println("잘못된 성별입니다.");
+        }
+    }
+}
